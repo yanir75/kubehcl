0ea42708cb5b429dccdba85643349df90eaa2f52
Yanir <yanircohen75@gmail.com>
2025-06-16 21:18:15 +0300

    fixed version

--- cli/version.go
+++ cli/version.go
@@ -4,18 +4,17 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
-
 )
 
 // Template prints the template which will be applied in yaml form after being rendered
-var version = "0.1.12"
+var version = "v0.1.12"
 
 func versionCmd() *cobra.Command {
 	versionCmd := &cobra.Command{
 		Use:   "version",
 		Short: "Shows the current version of the tool",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("Kuhbecl %s",version)
+			fmt.Printf("Kuhbecl %s", version)
 		},
 	}
 
