72a841a5acbd4bc080caed6ba576fadb57853ea4
Yanir <yanircohen75@gmail.com>
2025-08-31 15:15:30 +0300

    updated version

--- cli/version.go
+++ cli/version.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Template prints the template which will be applied in yaml form after being rendered
-var version = "v0.1.15"
+var version = "v0.1.17"
 
 func versionCmd() *cobra.Command {
 	versionCmd := &cobra.Command{
