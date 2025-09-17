203e438b35a02357ec790b72be6c024ae2316b8e
Yanir <yanircohen75@gmail.com>
2025-09-18 00:27:10 +0300

    changed version and modified from resource to kube_resource

--- cli/version.go
+++ cli/version.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Template prints the template which will be applied in yaml form after being rendered
-var version = "v0.1.19"
+var version = "v0.2.0"
 
 func versionCmd() *cobra.Command {
 	versionCmd := &cobra.Command{
